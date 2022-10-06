3e19b90054cac5a738b271dfab5aa4048427c0c0
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-06 15:01:23 +0300

    Add remove order functionality

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 import { menuArray } from './data.js';
+let orderlist = []
 
-const menu = document.getElementById('menu')
-// render out this of menu array to html
 
-function render() {
-    let menuitemshtml = ``
+// Target add to order button
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.add) {
+        addOrder(e.target.dataset.add);
+    } else if (e.target.dataset.remove) {
+        removeOrder(e.target.dataset.remove)
+    }
+})
+
+function addOrder(id) {
+    const orderItem = menuArray.filter((menuItem) => menuItem.id == id)[0]
+    orderlist.push(orderItem);
+    renderOrder()
+
+}
+
+function removeOrder(id) {
+    orderlist.forEach((order) => {
+        if (order.id == id) {
+            orderlist.splice(order, 1)
+            console.log("item removed")
+        }
+    })
+    console.log(orderlist)
+    renderOrder();
+}
+
+function getOrderHtml() {
+    let orderItemHtml = ``
+    orderlist.forEach((order) => {
+        orderItemHtml += `
+        <div class="order-item">
+            <div class="class-order-item-details">
+                <h5>${order.name}</h5>
+                <button class="remove-item" data-remove="${order.id}">remove</button>
+            </div>
+            <p>$ ${order.price}</p>
+        </div>
+        `
+    })
+
+    return orderItemHtml;
+}
+
+function renderOrder() {
+    document.getElementById('order-summary-list').innerHTML = getOrderHtml();
+}
+
+function getMenuHtml() {
+    let menuitemshtml = ``;
+
     menuArray.forEach((menuItem) => {
         menuitemshtml += `
         <div class="menu-item">
@@ -16,14 +64,19 @@ function render() {
                 <p>$ ${menuItem.price}</p>
             </div>
             <div>
-                <button class="add-item-btn"><i class="fa-regular fa-plus"></i></button>
+                <button class="add-item-btn" data-add="${menuItem.id}"><i class="fa-regular fa-plus"></i></button>
             </div>
         </div>
     </div>
         `
     });
 
-    menu.innerHTML = menuitemshtml;
+    return menuitemshtml;
+}
+
+
+function render() {
+    document.getElementById('menu').innerHTML = getMenuHtml();
 }
 
 render();
